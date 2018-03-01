29082030533c78f76a460a23951d9fb1cafd3b00
narayanpaul <36965289+narayanpaul@users.noreply.github.com>
2018-03-01 11:11:28 -0800

    add index column

--- analysis.py
+++ analysis.py
@@ -6,7 +6,7 @@ This is a temporary script file.
 """
 
 import pandas as pd
-data = pd.read_csv('data/gapminder_gdp_oceania.csv')
+data = pd.read_csv('data/gapminder_gdp_oceania.csv', index_col = 'country')
 
 
 
